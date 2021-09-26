f858ad0c3160be7b2884e5a3eeabc004e88f9960
Vladislav Hrushovyi <vlad.grush.99@gmail.com>
2021-09-26 11:28:30 +0300

    add new file

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
-<?php
-    $buttons_labs = [
-        "Перша лабораторна","Друга лабораторна",
-        "Третя лабораторна","Четверта лабораторна",
-        "П'ята лабораторна","Шоста лабораторна",
-        "Сьома лабораторна","Восьма лабораторна"
-    ];
-
-    foreach($buttons_labs as &$button){
-        echo "<p><a>$button</a></p>";
-    }
-?>
+<html>
+ <head>
+  <title>PHP Test</title>
+  <link rel="stylesheet" href="css/index.css">
+ </head>
+ <body>
+     <h1>Лабораторні роботи WEB</h1>
+     <div class="container">
+    <?php
+        $buttons_labs = [
+            "1-laba.php"=>"Перша лабораторна","2-laba.php"=>"Друга лабораторна",
+            "3-laba.php"=>"Третя лабораторна","4-laba.php"=>"Четверта лабораторна",
+            "5-laba.php"=>"П'ята лабораторна","6-laba.php"=>"Шоста лабораторна",
+            "7-laba.php"=>"Сьома лабораторна","8-laba.php"=>"Восьма лабораторна"
+        ];
+        ;
+        foreach($buttons_labs as $key => &$button){
+            echo "<a href=\"labs/$key\"> $button </a>";
+        }
+    ?>
+    </div>
+ </body>
+</html>
