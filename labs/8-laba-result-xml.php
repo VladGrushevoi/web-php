efe36580a5217e03de17b8c9f64979457886351d
Vlad Hrushovyi <vlad.grush.99@gmail.com>
2021-10-10 23:16:18 +0300

    8laba

--- labs/8-laba-result-xml.php
+++ labs/8-laba-result-xml.php
@@ -2,6 +2,12 @@
     $remote_ip = $_SERVER['REMOTE_ADDR'];
     $path = "http://ip-api.com/xml/$remote_ip";
     $xml = simplexml_load_file($path);
+
+    $s .= "";
+
+    foreach($xml as $key => $value){
+        $s.= $key ."--". $value . "\n";
+    }
     
-    echo $xml-> country_name;
+    echo $s;
 ?>
