39408282bb184a22e95fe1790c391518ea990206
Vlad Hrushovyi <vlad.grush.99@gmail.com>
2021-10-14 21:02:20 +0300

    8laba

--- js/8-laba.js
+++ js/8-laba.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $.get("8-laba-result-xml.php", function(data, status){
         console.log(data)
-        $("#result-xml").text(data + " " + status);
+        $("#result-xml").html(data);
     })
     $( "#form" ).submit(function( event ) {
         event.preventDefault();
--- labs/8-laba-result-xml.php
+++ labs/8-laba-result-xml.php
@@ -1,13 +1,14 @@
 <?php 
-    $remote_ip = $_SERVER['REMOTE_ADDR'];
+    // $remote_ip = $_SERVER['REMOTE_ADDR'];
+    $remote_ip = "109.227.106.117";
     $path = "http://ip-api.com/xml/$remote_ip";
     $xml = simplexml_load_file($path);
 
-    $s = "";
+    $html = "<p>Flag: <img src='../flags_ISO_3166-1/".strtolower($xml->countryCode).".png' alt='../flags_ISO_3166-1/_unitednations.png'></p>";
 
     foreach($xml as $key => $value){
-        $s.= $key ."--". $value . "\n";
+        $html .= "<p>". $key . ": ". $value ."</p>";
     }
     
-    echo $remote_ip;
+    echo $html;
 ?>
