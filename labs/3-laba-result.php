3a7a155e52e1c4c7b793dde71bdf1d2c917062ba
Vlad Hrushovyi <vlad.grush.99@gmail.com>
2021-10-15 12:49:09 +0300

    fix

--- labs/3-laba-result.php
+++ labs/3-laba-result.php
@@ -8,39 +8,39 @@
 
 <body>
     <?php
-    $choose = $_POST["course"];
-    echo $choose . "<a href='3-laba.php'> Назад до вибору</a>";
+    // $choose = $_POST["course"];
+    // echo $choose . "<a href='3-laba.php'> Назад до вибору</a>";
     // $correct_choose = strtolower(str_replace(" ", '', $choose));
-    $correct_choose = strtolower($choose);
-    $file = fopen("../data/data.txt", "rb");
-    $data = [];
+    // $correct_choose = strtolower($choose);
+    // $file = fopen("../data/data.txt", "rb");
+    // $data = [];
 
-    while (!feof($file)) {
-            $name_napr = strtolower(fgets($file));
-            if(strcmp($name_napr, $correct_choose) == 0){
-                $count_napr = intval(fgets($file));
-                $index = 1;
-                while($count_napr != $index){
-                    $avg_ball = doubleval(fgets($file));
-                    $count_free = intval(fgets($file));
-                    $nedobor = "-";
-                    $count_contr = intval(fgets($file));
-                    $correct_contr = $count_contr <= 0 ? "-" : $count_contr;
-                    $name_university = fgets($file);
+    // while (!feof($file)) {
+    //         $name_napr = strtolower(fgets($file));
+    //         if(strcmp($name_napr, $correct_choose) == 0){
+    //             $count_napr = intval(fgets($file));
+    //             $index = 1;
+    //             while($count_napr != $index){
+    //                 $avg_ball = doubleval(fgets($file));
+    //                 $count_free = intval(fgets($file));
+    //                 $nedobor = "-";
+    //                 $count_contr = intval(fgets($file));
+    //                 $correct_contr = $count_contr <= 0 ? "-" : $count_contr;
+    //                 $name_university = fgets($file);
 
-                    $object = array(
-                        'index' => $index,
-                        'count_free' => $count_free,
-                        'avg_ball' => $avg_ball,
-                        'nedobor' => $nedobor,
-                        'correct_contr' => $correct_contr,
-                        'name_university' => $name_university
-                    );
-                    array_push($data, $object);
-                    $index++;
-                }
-            }
-        }
+    //                 $object = array(
+    //                     'index' => $index,
+    //                     'count_free' => $count_free,
+    //                     'avg_ball' => $avg_ball,
+    //                     'nedobor' => $nedobor,
+    //                     'correct_contr' => $correct_contr,
+    //                     'name_university' => $name_university
+    //                 );
+    //                 array_push($data, $object);
+    //                 $index++;
+    //             }
+    //         }
+    //     }
 
 
         // if (strcmp($curr_line, $correct_choose) == 0) {
@@ -70,8 +70,8 @@
         //     }
         // }
     
-    fclose($file);
-    $_POST['data'] = $data;
+    // fclose($file);
+    // $_POST['data'] = $data;
     ?>
 
     <table>
@@ -84,29 +84,34 @@
             <th>ВНЗ</th>
         </tr>
         <?php
-        foreach ($_POST['data'] as $value) {
-            
-            echo "<tr>
-                <td>
-                   ". $value['index'] ."
-                </td>
-                <td>
-                ". $value['avg_ball'] ."
-                </td>
-                <td>
-                ". $value['count_free'] ."
-                </td>
-                <td>
-                ". $value['nedobor'] ."
-                </td>
-                <td>
-                ". $value['correct_contr'] ."
-                </td>
-                <td>
-                ". $value['name_university'] ."
-                </td>
-                </tr>";
+        $file = fopen("../data/data.txt", 'rb');
+        $specialty = $_POST["course"];
+    
+        while (!feof($file)) {
+            $string = trim(fgets($file));
+            if (strcasecmp($string, trim($specialty)) == 0) {
+                $rowNumber = 1;
+                $university_count = (int)fgets($file);
+                while (!feof($file) && $rowNumber <= $university_count) {
+                    $average_grade = fgets($file);
+                    $free_education_students_count = fgets($file);
+                    $pay_education_students_count = fgets($file);
+                    $university_name = fgets($file);
+                    $correct_contr = intval($pay_education_students_count) <= 0 ? '-': intval($pay_education_students_count);
+                    echo "<tr>
+                            <td>$rowNumber</td>
+                            <td>$average_grade</td>
+                            <td>$free_education_students_count</td>
+                            <td>-</td>
+                            <td>$correct_contr</td>
+                            <td>$university_name</td>
+                          </tr>";
+                    ++$rowNumber;
+                }
+                break;
+            }
         }
+        fclose($file);
         ?>
     </table>
 </body>
